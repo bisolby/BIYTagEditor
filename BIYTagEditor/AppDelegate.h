7b831d2b5ddccc19b9d5107f05556d2d60e0101b
Heejae Kim <lp90122@linecorp.com>
2016-05-02 12:10:10 +0900

    add project

---
+++ BIYTagEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BIYTagEditor
+//
+//  Created by mulberry on 2016. 5. 2..
+//  Copyright © 2016년 bisolby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BIYTagEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BIYTagEditor
+//
+//  Created by mulberry on 2016. 5. 2..
+//  Copyright © 2016년 bisolby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
